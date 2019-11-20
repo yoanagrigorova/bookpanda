0adac0f63668b3d25c5240c6860b2eec12df661d
Yoana Grigorova <grigorovajoana@gmail.com>
2019-11-20 21:43:08 +0200

    React/Redux setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
+import { simpleAction } from './actions/action';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const mapStateToProps = state => ({
+  ...state
+});
+
+const mapDispatchToProps = dispatch => ({
+  simpleAction: () => dispatch(simpleAction())
+});
+
+class App extends Component {
+  simpleAction = (event) => {
+    this.props.simpleAction();
+    console.log(this.props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload
+    </p>
+        <button onClick={this.simpleAction}>Test redux action</button>
+        <pre>
+          {
+            JSON.stringify(this.props)
+          }
+        </pre>
+      </div>
+    );
+  }
 }
-
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux';
+import simpleReducer from './reducer';
+export default combineReducers({
+    simpleReducer
+});
