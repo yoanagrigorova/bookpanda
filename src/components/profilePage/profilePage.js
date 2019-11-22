4ba8afc72e7723ccbd2cece4d704ca2429ffcf53
Avgustina Georgieva <avigeorgieva@astea.net>
2019-11-22 18:17:58 +0200

    Added basic routing in the navigation bar

---
+++ src/components/profilePage/profilePage.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+
+class profilePage extends Component {
+    
+    render() {
+          return(
+            <div className="profilePage">
+
+                Profile page goes here!
+            </div>
+          )
+      }
+}
+
+export default profilePage
