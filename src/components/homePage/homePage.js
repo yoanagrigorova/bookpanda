20b67f14a03ba88ab6bc0bea4b0df3a59eb3af7f
Avgustina Georgieva <avigeorgieva@astea.net>
2019-11-26 17:59:13 +0200

    Added post card component and added responsiveness

--- src/components/homePage/homePage.js
+++ src/components/homePage/homePage.js
@@ -3,12 +3,30 @@ import {HashRouter as Router, Switch, Link} from 'react-router-dom';
 import { Route } from 'react-router';
 
 import ProfilePage from '../profilePage/profilePage';
+import MenuIcon from '@material-ui/icons/Menu';
 
 
 import './homePage.css';
 
 class homePage extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.toggleMenu = this.toggleMenu.bind(this);
+    }
     
+    toggleMenu() {
+        console.log("hey")
+        const toggleMenu = document.querySelector('.homePage .header .navigation .hamburgerButton');
+        const menu = document.querySelector('.homePage .navigationPhone');
+
+        const open = JSON.parse(toggleMenu.getAttribute('aria-expanded'));
+        console.log(open);
+        toggleMenu.setAttribute('aria-expanded', !open);
+        menu.hidden = !menu.hidden;
+    }
+
     render() {
           return(
             <div className="homePage">
@@ -20,9 +38,20 @@ class homePage extends Component {
                             <li><Link to="/homePage/alPage">All</Link></li>
                             <li><Link to="/homePage/feedPage">Feed</Link></li>
                         </ul>
+                        <button
+                            className="hamburgerButton"
+                            onClick={this.toggleMenu}
+                        ><MenuIcon></MenuIcon>
+                        </button>
                     </nav>
                 </header>
-
+                <nav className="navigationPhone">
+                    <ul className="navigationListPhone">
+                        <li><Link to="/homePage/profilePage">My profile</Link></li>
+                        <li><Link to="/homePage/alPage">All</Link></li>
+                        <li><Link to="/homePage/feedPage">Feed</Link></li>
+                    </ul>
+                    </nav>
                 <Router>
                     <Switch>
                         <Route path="/homePage/profilePage" component={ProfilePage} />
